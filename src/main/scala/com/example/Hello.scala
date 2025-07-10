268c38bd5d8bac80038e4a0a1db5ed513a49cbe4
231tr0n <zeltronsrikar@gmail.com>
2025-07-11 02:11:16 +0530

    test

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,15 +1,16 @@
 package com.example
 
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SparkSession
 
 import scala.io.Source
 
 object Hello extends App {
-  val source = Source
+  val source: Iterator[String] = Source
     .fromResource("customers-2000000.csv")
     .getLines()
 
-  val spark = SparkSession
+  val spark: SparkSession = SparkSession
     .builder()
     .appName("Hello")
     .enableHiveSupport()
@@ -17,7 +18,7 @@ object Hello extends App {
 
   spark.sparkContext.setLogLevel("ERROR")
 
-  val df = spark.read.csv("customers-2000000.csv")
+  val df: DataFrame = spark.read.csv("customers-2000000.csv")
 
   df.groupBy("Country", "City")
     .count()
